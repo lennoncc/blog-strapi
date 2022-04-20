609b666b61be970603c4ca12cfb26077436abbff
lennoncc <lencruz@ucdavis.edu>
2022-04-19 23:39:25 -0700

    initial commit

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// } nextConfig
+
+module.exports = {
+  images: {
+    domains: ['localhost'],
+  },
+}
