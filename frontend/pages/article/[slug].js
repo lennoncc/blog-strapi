98586c01df0ebc27b8eadc13bb8e86238994e007
lennoncc <lencruz@ucdavis.edu>
2022-04-20 09:11:54 -0700

    nvm back to static

--- frontend/pages/article/[slug].js
+++ frontend/pages/article/[slug].js
@@ -85,7 +85,7 @@ export async function getStaticPaths() {
   };
 }
 
-export async function getServerSideProps({ params }) {
+export async function getStaticProps({ params }) {
   const articlesRes = await fetchAPI("/articles", {
     filters: {
       slug: params.slug,
