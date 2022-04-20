f8cdc6485f22ba239e46e4c5b13080906b3b7088
lennoncc <lencruz@ucdavis.edu>
2022-04-20 03:06:12 -0700

    fixed error checking

--- frontend/pages/article/[slug].js
+++ frontend/pages/article/[slug].js
@@ -93,7 +93,7 @@ export async function getStaticProps({ params }) {
     populate: ["image", "category", "author.picture"],
   });
   const categoriesRes = await fetchAPI("/categories");
-  if (!res.ok) {
+  if (!articlesRes.ok || !categoriesRes.ok) {
     // If there is a server error, you might want to
     // throw an error instead of returning so that the cache is not updated
     // until the next successful request.
